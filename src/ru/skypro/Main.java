08fa5055f9405558ada9e6f02520ae61a3843b31
Gabriel_n3 <p705865@gmail.com>
2022-06-12 17:47:41 +0300

    HW - 2

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,31 +1,35 @@
 package ru.skypro;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
+    private static final List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+    private static final List<String> words = new ArrayList<>(List.of("park", "sea", "park", "beach", "town", "sea"));
+
+
 
     public static void main(String[] args) {
-        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
         for (int n: nums) {
             if (n % 2 == 1) {
                 System.out.print(" " + n);
             }
         }
         System.out.println();
-        for (int n: nums) {
-            if (n % 2 != 1) {
-                System.out.print(" " + n);
-            }
+        Collections.sort(nums);
+        int min = Integer.MIN_VALUE;
+        for (int num : nums){
+           if (num %2 == 0 && num != min) {
+               System.out.print(" " + num);
+               min = num;
+
+           }
         }
         System.out.println();
 
-        List<String> words = new ArrayList<>(List.of("park", "sea", "park", "beach", "town", "sea"));
         Set<String> duble = new HashSet<>(words);
         System.out.println("без дублей" + duble);
-        
+
+        System.out.println(words.size()- duble.size());
     }
 
 }
